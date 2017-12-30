3485dbffa3371fc5ce67979804df99337137cdb4
Bartek <gekarez@gmail.com>
2017-12-30 16:06:55 +0100

    Removed FXML and class Controller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,18 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) {
+        GridPane gridPane = new GridPane();
+        Scene scene = new Scene(gridPane);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
